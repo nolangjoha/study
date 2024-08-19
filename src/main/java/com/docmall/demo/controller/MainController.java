4dbd468a4155b52e45dc3b829a8d2fa18f23e9e0
nolangjoha <nolangjoha@gmail.com>
2024-08-19 16:51:09 +0900

    스프링부트 프로젝트 서버배포2

--- src/main/java/com/docmall/demo/controller/MainController.java
+++ src/main/java/com/docmall/demo/controller/MainController.java
@@ -10,6 +10,6 @@ public class MainController {
 	@GetMapping("/")
 	@ResponseBody
 	public String rootUrl() {
-		return "<h3>스프링부트 배포 성공. - 수정배포4</h3>";
+		return "<h3>스프링부트 배포 성공. - 수정배포5</h3>";
 	}
 }
